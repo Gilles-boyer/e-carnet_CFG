9c66f471477188f676e9045c85d55c9183118c58
Gilles974 <boyer.gilles@live.fr>
2021-08-05 06:48:37 +0400

    finalisation BDD

---
+++ back/app/Http/Controllers/ChildrenNoteActivitieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChildrenNoteActivitie;
+use Illuminate\Http\Request;
+
+class ChildrenNoteActivitieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ChildrenNoteActivitie  $childrenNoteActivitie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChildrenNoteActivitie $childrenNoteActivitie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ChildrenNoteActivitie  $childrenNoteActivitie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChildrenNoteActivitie $childrenNoteActivitie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ChildrenNoteActivitie  $childrenNoteActivitie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChildrenNoteActivitie $childrenNoteActivitie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ChildrenNoteActivitie  $childrenNoteActivitie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChildrenNoteActivitie $childrenNoteActivitie)
+    {
+        //
+    }
+}
